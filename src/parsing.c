4ad233dc6cf6122d636d05287c7396c1d435cb77
Filip Tijan <ftijan@gmail.com>
2021-03-26 22:27:23 +0000

    error handling, reducing ops

--- src/parsing.c
+++ src/parsing.c
@@ -26,6 +26,57 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+enum { LVAL_NUM, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+/* number type lval */
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+/* error type lval */
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+/* Print an "lval" */
+void lval_print(lval v) {
+  switch (v.type) {
+    /* In the case the type is a number print it */
+    /* Then 'break' out of the switch. */
+    case LVAL_NUM: printf("%li", v.num); break;
+
+    /* In the case the type is an error */
+    case LVAL_ERR:
+      /* Check what type of error it is and print it */
+      if (v.err == LERR_DIV_ZERO) {
+        printf("Error: Division By Zero!");
+      }
+      if (v.err == LERR_BAD_OP)   {
+        printf("Error: Invalid Operator!");
+      }
+      if (v.err == LERR_BAD_NUM)  {
+        printf("Error: Invalid Number!");
+      }
+    break;
+  }
+}
+
+/* Print an "lval" followed by a newline */
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
 /* Test: recursive node count */
 int number_of_nodes(mpc_ast_t* t) {
     if (t->children_num == 0) { return 1; }
@@ -44,28 +95,39 @@ void node_number_print(mpc_ast_t* ast) {
     printf("Number of nodes: %d\n", node_number);
 }
 
-/* eval */
-double eval_op(double x, char* op, double y) {
-    if(strcmp(op, "+") == 0) { return x + y; };
-    if(strcmp(op, "-") == 0) { return x - y; };
-    if(strcmp(op, "*") == 0) { return x * y; };
-    if(strcmp(op, "/") == 0) { return x / y; };
-    if(strcmp(op, "%") == 0) { return (long)x % (long)y; };
-    if(strcmp(op, "^") == 0) { return pow(x, y); };
-    return 0;
+lval eval_op(lval x, char* op, lval y) {
+
+  /* If either value is an error return it */
+  if (x.type == LVAL_ERR) { return x; }
+  if (y.type == LVAL_ERR) { return y; }
+
+  /* Otherwise do maths on the number values */
+  if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+  if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+  if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+  if (strcmp(op, "/") == 0) {
+    /* If second operand is zero return error */
+    return y.num == 0
+      ? lval_err(LERR_DIV_ZERO)
+      : lval_num(x.num / y.num);
+  }
+
+  return lval_err(LERR_BAD_OP);
 }
 
-double eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
     /* return value if number */
     if (strstr(t->tag, "number")) {
-        return strtod(t->contents, NULL);
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);        
     }
 
     /* operator */
     char* op = t->children[1]->contents;
-
+    
     /* store third child in 'x' */
-    double x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     int i = 3;
     while(strstr(t->children[i]->tag, "expr")) {
@@ -87,7 +149,7 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
         "\
         number  : /-?([0-9]+[.])?[0-9]+/ ; \
-        operator: '+' | '-' | '*' | '/' | '%' | '^' ; \
+        operator: '+' | '-' | '*' | '/' ; \
         expr    : <number> | '(' <operator> <expr>+ ')' ; \
         lispy   : /^/ <operator> <expr>+ /$/ ; \
         ",
@@ -108,8 +170,8 @@ int main(int argc, char** argv) {
             //mpc_ast_print(r.output);                       
             //node_number_print(r.output);
 
-            double result = eval(r.output);
-            printf("%lf\n", result);
+            lval result = eval(r.output);
+            lval_println(result);            
             
             mpc_ast_delete(r.output);
         } else {
