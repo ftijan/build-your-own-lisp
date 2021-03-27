28f3c45410eaa4d97a37fea94f7e839c08d77924
Filip Tijan <ftijan@gmail.com>
2021-03-27 03:19:37 +0000

    added q-expression functions

--- src/parser-util.c
+++ src/parser-util.c
@@ -1,5 +1,8 @@
 #include "parser-util.h"
 
+#define LASSERT(args, cond, err) \
+    if (!(cond)) { lval_del(args); return lval_err(err); }
+
 /* number type lval */
 lval* lval_num(long x) {
     lval* v = malloc(sizeof(lval));
@@ -151,7 +154,7 @@ lval* lval_eval_sexpr(lval* v) {
     }
 
     /* built-in operator call */
-    lval* result = builtin_op(v, f->sym);
+    lval* result = builtin(v, f->sym);
     lval_del(f);
     return result;
 }
@@ -215,4 +218,75 @@ lval* builtin_op(lval* a, char* op) {
     
     lval_del(a);
     return x;
+}
+
+lval* builtin_head(lval* a) {
+    LASSERT(a, a->count == 1, "Function 'head' passed too many arguments.");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' passed incorrect types.");
+    LASSERT(a, a->cell[0]->count != 0, "Function 'head' passed {}.");
+
+    lval* v = lval_take(a, 0);
+
+    while(v->count > 1) { lval_del(lval_pop(v, 1)); }
+    return v;
+}
+
+lval* builtin_tail(lval* a) {
+    LASSERT(a, a->count == 1, "Function 'tail' passed too many arguments.");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'tail' passed incorrect types.");
+    LASSERT(a, a->cell[0]->count != 0, "Function 'tail' passed {}.");
+
+    lval* v = lval_take(a, 0);
+
+    lval_del(lval_pop(v, 0));
+    return v;
+}
+
+lval* builtin_list(lval* a) {
+    a->type = LVAL_QEXPR;
+    return a;
+}
+
+lval* builtin_eval(lval* a) {
+    LASSERT(a, a->count == 1, "Function 'eval' passed too many arguments.");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'eval' passed incorrect types.");
+
+    lval* x = lval_take(a, 0);
+    x->type = LVAL_SEXPR;
+    return lval_eval(x);
+}
+
+lval* builtin_join(lval* a) {
+    for (int i = 0; i < a->count; i++) {
+        LASSERT(a, a->cell[i]->type == LVAL_QEXPR, "Function 'join' passed incorrect type.")
+    }
+
+    lval* x = lval_pop(a, 0);
+
+    while (a->count) {
+        x = lval_join(x, lval_pop(a, 0));
+    }
+
+    lval_del(a);   
+    return x;
+}
+
+lval* lval_join(lval* x, lval* y) {
+    while (y->count) {
+        x = lval_add(x, lval_pop(y, 0));
+    }
+    
+    lval_del(y);
+    return x;
+}
+
+lval* builtin(lval* a, char* func) {
+    if (strcmp("list", func) == 0) { return builtin_list(a); }
+    if (strcmp("head", func) == 0) { return builtin_head(a); }
+    if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+    if (strcmp("join", func) == 0) { return builtin_join(a); }    
+    if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+    if (strstr("+-/*", func)) { return builtin_op(a, func); }
+    lval_del(a);
+    return lval_err("Unknown function.");
 }
