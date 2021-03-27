6d13e8ba419c9703904974335f5a38347685f487
Filip Tijan <ftijan@gmail.com>
2021-03-27 23:31:27 +0000

    added args parsing for file load

--- src/parsing.c
+++ src/parsing.c
@@ -41,33 +41,49 @@ int main(int argc, char** argv) {
         ",
         Number, Symbol, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
-    /* Print Version and Exit Information */
-    puts("Lispy Version 0.0.0.1");
-    puts("Press Ctrl+c to Exit\n");
-
     lenv* e = lenv_new();
     lenv_add_builtins(e);
 
-    while (1) {
-        char* input = readline("lispy> ");
-        add_history(input);
+    /* repl */
+    if (argc == 1) {        
+        puts("Lispy Version 0.0.0.1");
+        puts("Press Ctrl+c to Exit\n");
 
-        /* Parse the input */
-        mpc_result_t r;
-        if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            /* Success: print and delete AST */
-            lval* x = lval_eval(e, lval_read(r.output));
-            lval_println(x);
-            lval_del(x);
-            
-            mpc_ast_delete(r.output);
-        } else {
-            /* Fail: print and delete the error */
-            mpc_err_print(r.error);
-            mpc_err_delete(r.error);
+        while (1) {
+            char* input = readline("lispy> ");
+            add_history(input);
+
+            /* Parse the input */
+            mpc_result_t r;
+            if (mpc_parse("<stdin>", input, Lispy, &r)) {
+                /* Success: print and delete AST */
+                lval* x = lval_eval(e, lval_read(r.output));
+                lval_println(x);
+                lval_del(x);
+                
+                mpc_ast_delete(r.output);
+            } else {
+                /* Fail: print and delete the error */
+                mpc_err_print(r.error);
+                mpc_err_delete(r.error);
+            }
+
+            free(input);
         }
+    }
 
-        free(input);
+    /* file list args */
+    if (argc >= 2) {
+        /* for each file name */
+        for (int i = 1; i < argc; i++) {
+            /* filename */
+            lval* args = lval_add(lval_sexpr(), lval_str(argv[i]));
+
+            lval* x = builtin_load(e, args);
+
+            if(x->type == LVAL_ERR) { lval_println(x); }
+            lval_del(x);
+        }        
     }
 
     lenv_del(e);
