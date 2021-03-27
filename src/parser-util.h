e6d427a70ec0437c26910a0ca861823129d92462
Filip Tijan <ftijan@gmail.com>
2021-03-27 18:55:34 +0000

    user funcs, eval scopes

--- src/parser-util.h
+++ src/parser-util.h
@@ -12,17 +12,25 @@ typedef lval*(*lbuiltin)(lenv*, lval*);
 
 struct lval {
     int type;
-    long num;
-    /* string data in Error and Symbol */
+
+    /* basic */
+    long num;    
     char* err;
     char* sym;
-    lbuiltin fun;
-    /* count and pointer to a list of lval* */
+
+    /* function */
+    lbuiltin builtin;
+    lenv* env;
+    lval* formals;
+    lval* body;
+    
+    /* expression */
     int count;
-    struct lval** cell;
+    lval** cell;
 };
 
 struct lenv {
+    lenv* par;
     int count;
     char** syms;
     lval** vals;
@@ -64,4 +72,11 @@ lval* builtin_div(lenv* e, lval* a);
 void lenv_add_builtin(lenv* e, char* name, lbuiltin func);
 void lenv_add_builtins(lenv* e);
 char* ltype_name(int t);
+lval* builtin_var(lenv* e, lval* a, char* func);
+lval* lval_lambda(lval* formals, lval* body);
+lval* builtin_lambda(lenv* e, lval* a);
+lenv* lenv_copy(lenv* e);
+void lenv_def(lenv* e, lval* k, lval* v);
 lval* builtin_def(lenv* e, lval* a);
+lval* builtin_put(lenv* e, lval* a);
+lval* lval_call(lenv* e, lval* f, lval* a);
